6874a8cf87e073bed15943eda4a51de4c7477068
HIMANSHU-heist <bendalehimanshu5@gmail.com>
2025-10-20 09:35:06 +0530

    Add files via upload

---
+++ pythonbasic/numbers.py
@@ -0,0 +1,6 @@
+# Taking input as int
+# Typecasting to int
+n = int(input("Enter an integer: "))
+print((n*8) + 10)
+n = float(input("Enter an integer: "))
+print((n*8) + 10)
---
+++ pythonbasic/variables.py
@@ -0,0 +1,9 @@
+# Single variable
+A = " hello himanshu"
+print(A)
+
+# Multiple Variables
+B = "hello"
+age = 25
+city = "california"
+print(B, age, city)
